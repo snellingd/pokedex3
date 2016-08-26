fbed9cc21e3057ff78b359a2d20b3b055b36b8dc
Duane Snelling <duane.snelling@icloud.com>
2016-08-26 12:56:42 -0400

    add Collection cell loaded data
    loaded the pokemon in to cells in collection view.  configured the
    cells appearance.

---
+++ Pokedex3/PokeCell.swift
@@ -0,0 +1,33 @@
+//
+//  PokeCell.swift
+//  Pokedex3
+//
+//  Created by Duane Snelling on 2016-08-26.
+//  Copyright © 2016 Duane Snelling. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    
+    @IBOutlet weak var thumbImg: UIImageView!
+    @IBOutlet weak var nameLbl: UILabel!
+    
+    var pokemon: Pokemon!
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        layer.cornerRadius = 5.0
+        
+    }
+    
+    func configureCell(pokemon: Pokemon) {
+        self.pokemon = pokemon
+        
+        nameLbl.text = self.pokemon.name.capitalized
+        thumbImg.image = UIImage(named: "\(self.pokemon.pokedexID)")
+        
+    }
+    
+}
