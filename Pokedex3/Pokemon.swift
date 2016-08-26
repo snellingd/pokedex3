4db847bbe3f8ba60cbd8d952f12cb1064ecdf984
Duane Snelling <duane.snelling@icloud.com>
2016-08-26 12:17:47 -0400

    created first part of view
    added nav, search, collection, cell

---
+++ Pokedex3/Pokemon.swift
@@ -0,0 +1,29 @@
+//
+//  Pokemon.swift
+//  Pokedex3
+//
+//  Created by Duane Snelling on 2016-08-26.
+//  Copyright © 2016 Duane Snelling. All rights reserved.
+//
+
+import Foundation
+
+class Pokemon {
+    
+    private var _name: String!
+    private var _pokedexID: Int!
+    
+    var name: String {
+        return _name
+    }
+    
+    var pokedexID: Int {
+        return _pokedexID
+    }
+    
+    init(name: String, pokedexID: Int) {
+        self._name = name
+        self._pokedexID = pokedexID
+    }
+    
+}
